10499a1acae27404916a93d2760f86d02950d44e
Jonathan AhNee <jonahnee@gmail.com>
2015-11-05 23:32:25 -1000

    test cases pass

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secretNumber = (Math.floor(Math.random() * 1000000 ));
+
+  var ranSecretNumber = function () {
+    return secretNumber;
+  };
+
+  return ranSecretNumber;
+
 };
