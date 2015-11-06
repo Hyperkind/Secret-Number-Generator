9da60ab45a4d336350e5fcfc0c976a871627fabb
Jonathan AhNee <jonahnee@gmail.com>
2015-11-06 00:23:57 -1000

    added comments to completed work

--- secret_number.js
+++ secret_number.js
@@ -2,11 +2,13 @@
 module.exports = function() {
 
   var secretNumber = (Math.floor(Math.random() * 1000000 ));
+  // sets secretNumber to a random number from 0 - 1000000
 
   var ranSecretNumber = function () {
     return secretNumber;
+  // function that returns secretNumber as the same number every time
   };
 
   return ranSecretNumber;
-
+  // invokes the function
 };
