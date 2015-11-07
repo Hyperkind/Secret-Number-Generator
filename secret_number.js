82bae5ec097d524a2beae798de4a405a60d4c316
Jonathan AhNee <jonahnee@gmail.com>
2015-11-07 11:25:08 -1000

    all tests pass

--- secret_number.js
+++ secret_number.js
@@ -1,14 +1,17 @@
 'use strict';
 module.exports = function() {
 
-  var secretNumber = (Math.floor(Math.random() * 1000000 ));
-  // sets secretNumber to a random number from 0 - 1000000
+  var secretNumber = (Math.floor(Math.random() * 1000001 ));
 
   var ranSecretNumber = function () {
     return secretNumber;
-  // function that returns secretNumber as the same number every time
   };
-
   return ranSecretNumber;
-  // invokes the function
 };
+
+// Jessie's example
+// var secretNumber;
+// return function () {
+// return secretNumber = secretNumber || Math.floor(Math.random() * 1000001);
+//    first run of this line since secretNumber is undefined will run the or " || " to generate the random number
+// }; 
